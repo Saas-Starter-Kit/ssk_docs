2f599e4dbfbd5f17e49d54d76ee1e2b008992b6a
iqbal125 <iqbal125@yahoo.com>
2021-05-29 13:21:40 -0700

    initial commit

---
+++ docusaurus.config.js
@@ -0,0 +1,44 @@
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
+module.exports = {
+  title: "React and Nodejs Boilerplate",
+  tagline: "React and Nodejs Boilerplate",
+  url: "https://saasstarterkit.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.ico",
+  organizationName: "Saas Starterkit",
+  projectName: "Saas-Starterkit",
+  themeConfig: {
+    navbar: {
+      title: "Saas Starter Kit Docs",
+      logo: {
+        alt: "My Site Logo",
+        src: "img/logo.svg",
+        href: "/docs/intro",
+      },
+      items: [
+        {
+          href: "https://github.com/Saas-Starter-Kit",
+          className: "header-github-link",
+          position: "right",
+        },
+      ],
+    },
+  },
+  presets: [
+    [
+      "@docusaurus/preset-classic",
+      {
+        docs: {
+          sidebarPath: require.resolve("./sidebars.js"),
+          // Please change this to your repo.
+          editUrl: "https://github.com/Saas-Starter-Kit/ssk_docs",
+        },
+        theme: {
+          customCss: require.resolve("./src/css/custom.css"),
+        },
+      },
+    ],
+  ],
+}
